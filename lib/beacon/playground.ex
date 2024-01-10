91d710a17cabbbf8281fd4b79672eed4b1e88676
Eduardo Borsa <borsa.dado@gmail.com>
2024-01-10 17:03:35 -0300

    foo
    Signed-off-by: Eduardo Borsa <borsa.dado@gmail.com>

--- lib/beacon/playground.ex
+++ lib/beacon/playground.ex
@@ -21,7 +21,10 @@ defmodule Beacon.Playground do
           <div class="xl:absolute xl:top-10 xl:left-0 xl:w-full 2xl:top-20 px-4">
             <div class="flex flex-col max-w-4xl mx-auto text-center">
               <div class="sm:absolute sm:top-10 sm:left-0 sm:w-full xl:static xl:top-auto xl:left-auto flex flex-col">
-                <BeaconWeb.Components.featured_pages />
+                <pre> <%= inspect( Beacon.Content.list_pages(:dev, per_page: 3), struct: false, pretty: true) %></pre>
+                <BeaconWeb.Components.unordered_list :let={fruit} entries={Beacon.Content.list_pages(:dev, per_page: 3)}>
+                  I like <b><%= fruit.title %></b>!
+                </BeaconWeb.Components.unordered_list>
               </div>
             </div>
           </div>
